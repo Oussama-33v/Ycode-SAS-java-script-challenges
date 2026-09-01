2d70fa81bc2a260ba5d5a7655a03412251f2f2fd
Oussama Akhrraz <oussamaakhraz173@gmail.com>
2026-09-01 16:08:16 +0100

    challenge 7

---
+++ Variables/challenge7.js
@@ -0,0 +1,14 @@
+const readline = require('node:readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+rl.question('enter Continuous assessment note :', function(noteCC){
+    rl.question('enter Continuous project note :', function(noteProjet){
+        rl.question('enter Continuous final exam note :', function(noteExamen){
+            let Notefinale = (noteCC * 2 + noteProjet * 3 + noteExamen * 5) / 10;
+            console.log('final note:' ,Notefinale);
+            rl.close();
+        });
+    });
+});
