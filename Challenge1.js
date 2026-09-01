f5403a5a0069a499f57fb73cf6d0d9ffef46e4e7
Oussama Akhrraz <oussamaakhraz173@gmail.com>
2026-09-01 09:33:16 +0100

    challenge 1, 2

---
+++ Challenge1.js
@@ -0,0 +1,13 @@
+// Créez un programme qui stocke dans des variables les informations d'un joueur :
+
+let Pseudo = "ShadowX";
+let Niveau = 12;
+let Age = 21;
+let Score = 4580;
+let Pays = "maroc";
+
+console.log("player name :",Pseudo);
+console.log("player level:",Niveau);
+console.log("player age  :",Age);
+console.log("player score :",Score);
+console.log("player country :",Pays);
---
+++ Challenge2.js
@@ -0,0 +1,7 @@
+//Vous développez une application permettant à un voyageur de convertir son budget.
+let MAD = 2200;
+let EUR = MAD / 11;
+
+console.log(EUR)
+
+
