fcb470285c8739fbdce21f4a6f0c97475cdd0369
Oussama Akhrraz <oussamaakhraz173@gmail.com>
2026-09-02 22:25:48 +0100

    challenge 4

--- loops/challenge4.js
+++ loops/challenge4.js
@@ -1,12 +1,19 @@
 //Write a program that calculates a power without using Math.pow().
 
 const prompt = require('prompt-sync')();
-let base = prompt('inter a base :');
-let exposant = prompt('inter an exposant :');
+let base = +prompt('inter a base :');
+let exposant = +prompt('inter an exposant :');
 let i = 1;
 let rusalt = 1
-while (i <= exposant) {
-   rusalt = rusalt * base;
-   i++;
+if (base === 0) {
+   console.log(0)
+   return 0;
+}
+else {
+   while (i <= exposant) {
+      rusalt = base * base;
+      i++;
+   }
 }
 console.log(rusalt);
+
