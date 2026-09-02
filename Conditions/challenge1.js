a77f15caef6bd5986e0227a9d311176123a25f62
Oussama Akhrraz <oussamaakhraz173@gmail.com>
2026-09-02 09:39:00 +0100

    challenge 1 if, else if

--- Conditions/challenge1.js
+++ Conditions/challenge1.js
@@ -1,12 +1,13 @@
 //An online store offers free delivery above a certain amount.
 
 const prompt = require('prompt-sync')();
-let amount = +prompt('haw much you spend sur !:');
-if(amount <= 500){
-    amount += 40;
+let amount = +prompt('haw much you spend sir !:');
+if(amount <= 500){ //Amount < 500 DH→ Delivery charges:40 DH
+        amount += 40;
     console.log("total to pay + delevry :", amount);
 }
 else{
-    console.log("free delevary");
+    console.log("free delevary"); // Amount >= 500 DH→ Free delivery
     console.log("toutal to pay is", amount);
 }
+console.log("have a good day sir")
